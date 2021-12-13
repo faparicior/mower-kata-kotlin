28380eccbcf39960803966fddf6232a4d51ca177
Fernando Aparicio <faparicior@gmail.com>
2021-12-13 19:04:09 +0100

    Add mower position VO

---
+++ src/main/kotlin/mower/mower/exception/InvalidMowerPositionException.kt
@@ -0,0 +1,12 @@
+package mower.mower.exception
+
+class InvalidMowerPositionException private constructor (message: String? = null, cause: Throwable? = null) : Exception(message, cause) {
+    companion object {
+        private const val MESSAGE: String = "Invalid Y position %d. Only positive values are valid."
+
+        fun withValue(value: Int): InvalidMowerPositionException
+        {
+            return InvalidMowerPositionException(String.format(MESSAGE, value), null)
+        }
+    }
+}
---
+++ src/test/kotlin/mower/mower/exception/InvalidMowerPositionExceptionTest.kt
@@ -0,0 +1,20 @@
+package mower.mower.exception
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import kotlin.test.Test
+
+private const val POSITION = 4
+private const val MESSAGE = "Invalid Y position $POSITION. Only positive values are valid."
+
+internal class InvalidMowerPositionExceptionTest
+{
+    @Test
+    fun `Should be build with custom value`() {
+        val exception = assertThrows(InvalidMowerPositionException::class.java) {
+            throw InvalidMowerPositionException.withValue(POSITION)
+        }
+
+        assertThat(exception.message).isEqualTo(MESSAGE)
+    }
+}
