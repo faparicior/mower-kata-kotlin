0491292b011aa24876c88971a117da86fe222a52
Fernando Aparicio <faparicior@gmail.com>
2021-12-20 17:53:01 +0100

    Mower position out of bounds scenario

--- src/test/kotlin/mower/mower/MowerTest.kt
+++ src/test/kotlin/mower/mower/MowerTest.kt
@@ -5,8 +5,13 @@ import mower.mower.value_object.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import java.util.*
+import java.util.stream.Stream
 import kotlin.test.Test
+import kotlin.test.assertContains
 
 private const val MOVE_LEFT: String = "L"
 private const val MOVE_RIGHT: String = "R"
@@ -25,6 +30,7 @@ internal class MowerTest {
     private lateinit var mower: Mower
     private val mowerId: MowerId = MowerId.build(UUID.randomUUID().toString())
     private lateinit var mowerPosition: MowerPosition
+    private lateinit var surface: Surface
 
     @BeforeEach
     fun setUp() {
@@ -37,6 +43,11 @@ internal class MowerTest {
         mower = Mower.build(
             mowerId,
             mowerPosition
+        )
+
+        surface = Surface.build(
+            SurfaceXSize.build(5),
+            SurfaceYSize.build(5)
         )
     }
 
@@ -56,7 +67,7 @@ internal class MowerTest {
     fun `Should be able to turn left`() {
         val expectedMowerOrientation = MowerOrientation.build(WEST_ORIENTATION)
 
-        mower.move(MowerMovement.build(MOVE_LEFT))
+        mower.move(MowerMovement.build(MOVE_LEFT), surface)
         assertThat(mower.mowerPosition().orientation).isEqualTo(expectedMowerOrientation)
     }
 
@@ -64,23 +75,30 @@ internal class MowerTest {
     fun `Should be able to turn right`() {
         val expectedMowerOrientation = MowerOrientation.build(EAST_ORIENTATION)
 
-        mower.move(MowerMovement.build(MOVE_RIGHT))
+        mower.move(MowerMovement.build(MOVE_RIGHT), surface)
         assertThat(mower.mowerPosition().orientation).isEqualTo(expectedMowerOrientation)
     }
 
     @Test
     fun `Should be able to move forward`() {
-        mower.move(MowerMovement.build(MOVE_FORWARD))
+        mower.move(MowerMovement.build(MOVE_FORWARD), surface)
         assertThat(mower.mowerPosition().xPosition.value).isEqualTo(X_POSITION)
         assertThat(mower.mowerPosition().yPosition.value).isEqualTo(Y_POSITION + STEP)
     }
 
-    @Test
-    fun `Should throw exception if goes out of bounds`() {
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("positionAndMovementProvider")
+    fun `Should throw exception if goes out of bounds`(
+        scenario: String,
+        xPosition: Int,
+        yPosition: Int,
+        orientation: String,
+        message: String
+    ) {
         val mowerPosition = MowerPosition.build(
-            XMowerPosition.build(X_POSITION),
-            YMowerPosition.build(Y_POSITION),
-            MowerOrientation.build(SOUTH_ORIENTATION)
+            XMowerPosition.build(xPosition),
+            YMowerPosition.build(yPosition),
+            MowerOrientation.build(orientation)
         )
 
         val mower = Mower.build(
@@ -88,10 +106,22 @@ internal class MowerTest {
             mowerPosition
         )
 
-        mower.move(MowerMovement.build("F"))
+        val exception = assertThrows(InvalidMowerPositionException::class.java) {
+            mower.move(MowerMovement.build(MOVE_FORWARD), surface)
+        }
 
-        assertThrows(InvalidMowerPositionException::class.java) {
-            mower.move(MowerMovement.build("F"))
+        assertContains(exception.message.toString(), message)
+    }
+
+    companion object {
+        @JvmStatic
+        fun positionAndMovementProvider(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.arguments("Out of bounds by negative value movement axis Y", 5, 0, SOUTH_ORIENTATION, "Only positive values are valid"),
+                Arguments.arguments("Out of bounds by negative value movement axis X", 0, 5, WEST_ORIENTATION, "Only positive values are valid"),
+                Arguments.arguments("Out of bounds movement axis Y", 0, 5, NORTH_ORIENTATION, "Out of bounds"),
+                Arguments.arguments("Out of bounds movement axis X", 5, 0, EAST_ORIENTATION, "Out of bounds")
+            )
         }
     }
 }
